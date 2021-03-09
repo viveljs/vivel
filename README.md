7c0a4a85d5c51e43f286740bf247703f0e7c3a09
Muhammad Tsany Qudsi <tsanyqudsi@gmail.com>
2021-03-09 19:01:55 +0700

    fix readme

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
+Icon by [Fi](https://instagram.com/afiifahputriiw)
+
 ## Getting Started
 
 First, run the development server:
--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -1,11 +1,20 @@
 import { FC } from 'react';
-import { Box, Center } from '@chakra-ui/react';
+import { Box, Center, Text } from '@chakra-ui/react';
 
 const Dialog: FC = ({ children }) => {
   return (
-    <Center w="full" zIndex={2} position="fixed" bottom={2}>
+    <Center w="full" zIndex={2} position="fixed" bottom={5}>
       <Box bg="steelblue" w="full" mx="6" px="7" py="4">
-        Dialog{children}
+        <Text color="white" fontWeight="bold">
+          Dialog
+        </Text>
+        <Text color="white">
+          At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
+          voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati
+          cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id
+          est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.
+          {children}
+        </Text>
       </Box>
     </Center>
   );
